8b1d84125a945c9c403b8bb4cae0fb6c24c8f57d
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2025-08-20 16:59:12 -0300

    Initial commit

---
+++ src/main/java/com/carolribeiro2112/controle_gastos_backend/ControleGastosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.carolribeiro2112.controle_gastos_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleGastosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleGastosBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carolribeiro2112/controle_gastos_backend/ControleGastosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carolribeiro2112.controle_gastos_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleGastosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
