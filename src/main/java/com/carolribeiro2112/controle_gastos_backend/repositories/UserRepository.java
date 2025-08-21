e48639a1ddb8d4413cb413a09f07f217732a6470
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2025-08-20 21:40:05 -0300

    [CDG-2][CDG-21] Add docker configuration

--- src/main/java/com/carolribeiro2112/controle_gastos_backend/repositories/UserRepository.java
+++ src/main/java/com/carolribeiro2112/controle_gastos_backend/repositories/UserRepository.java
@@ -1,7 +1,7 @@
 package com.carolribeiro2112.controle_gastos_backend.repositories;
 
+import com.carolribeiro2112.controle_gastos_backend.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, String> {
