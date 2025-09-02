79aeb3fb44e9318d051269a66ee95a37c69b5b7a
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2025-09-02 17:50:55 -0300

    [CDG-44][CDG-48]- anyRequest.authenticated()

--- src/main/java/com/carolribeiro2112/controle_gastos_backend/infra/security/SecurityConfigurations.java
+++ src/main/java/com/carolribeiro2112/controle_gastos_backend/infra/security/SecurityConfigurations.java
@@ -37,7 +37,7 @@ public class SecurityConfigurations {
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/transaction").hasRole("ADMIN")
-                        .anyRequest().permitAll()
+                        .anyRequest().authenticated()
                 )
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
