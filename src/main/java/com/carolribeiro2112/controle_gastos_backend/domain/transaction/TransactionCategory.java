26a6c79a7047522acee43cee293d2508bfa0335e
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2025-10-05 22:04:27 -0300

    [CDG-76][CDG-77] - Create ENUM for categories

---
+++ src/main/java/com/carolribeiro2112/controle_gastos_backend/domain/transaction/TransactionCategory.java
@@ -0,0 +1,16 @@
+package com.carolribeiro2112.controle_gastos_backend.domain.transaction;
+
+public enum TransactionCategory {
+    FOOD,
+    TRANSPORTATION,
+    UTILITIES,
+    ENTERTAINMENT,
+    HEALTHCARE,
+    EDUCATION,
+    PERSONAL_CARE,
+    GROCERIES,
+    RENT,
+    SALARY,
+    INVESTMENTS,
+    OTHER
+}
