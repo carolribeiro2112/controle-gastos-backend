094bc0c81dbd025d9c9372e52994f610b8e3f0a1
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2025-10-12 19:32:33 -0300

    [CDG-85][CDG-88] - Add add filters by type and category in Transaction service

--- src/main/java/com/carolribeiro2112/controle_gastos_backend/repositories/TransactionRepository.java
+++ src/main/java/com/carolribeiro2112/controle_gastos_backend/repositories/TransactionRepository.java
@@ -1,10 +1,23 @@
 package com.carolribeiro2112.controle_gastos_backend.repositories;
 
 import com.carolribeiro2112.controle_gastos_backend.domain.transaction.Transaction;
+import com.carolribeiro2112.controle_gastos_backend.domain.transaction.TransactionCategory;
+import com.carolribeiro2112.controle_gastos_backend.domain.transaction.TransactionType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction, String> {
-    List<Transaction> findByUserId(String userId);
+    @Query("SELECT t FROM Transaction t WHERE t.user.id = :userId " +
+            "AND (:category IS NULL OR t.category = :category) " +
+            "AND (:type IS NULL OR t.type = :type)")
+    List<Transaction> findByFilters(
+            @Param("userId") String userId,
+            @Param("category") TransactionCategory category,
+            @Param("type") TransactionType type
+    );
 }
